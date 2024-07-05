df4e93e59e6c8e3c3f6ac39f3fbc0277e3d467c2
tharindug99 <tharindunirmala99@gmail.com>
2024-07-05 19:22:38 +0530

    removed unnecessary imports

--- app/users/[id]/posts/page.tsx
+++ app/users/[id]/posts/page.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import PostsByUser from '@/components/PostsByUser';
-import { useParams } from 'react-router-dom';
-function Posts() {
-  
+import React from "react";
+import PostsByUser from "@/components/PostsByUser";
 
+function Posts() {
   return (
     <div className="flex flex-col items-center container mx-auto p-4 py-2">
-        <PostsByUser/>
+      <PostsByUser />
     </div>
   );
 }

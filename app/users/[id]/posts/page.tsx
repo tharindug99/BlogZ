59790cb30cae783d7cdc4d6c96a5b11604721b04
tharindug99 <tharindunirmala99@gmail.com>
2024-07-18 21:34:21 +0530

    error detection

--- app/users/[id]/posts/page.tsx
+++ app/users/[id]/posts/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PostsByUser from "@/components/PostsByUser";
-
+///Is it here
 function Posts() {
   return (
     <div className="flex flex-col items-center container mx-auto p-4 py-2">
